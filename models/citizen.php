912ac892241b2bd7cfb737dfb5f96c28baa4016f
Cmauricio Parra <cmauriciop@yahoo.com.mx>
2020-04-22 06:17:20 -0500

    Add destroy method to citizen model

--- models/citizen.php
+++ models/citizen.php
@@ -114,4 +114,25 @@ class Citizen {
 
     return false;
   }
+
+  function destroy() {
+    // delete query
+    $query = 'DELETE FROM ' . $this->table_name . ' WHERE id = ?';
+
+    // prepare query
+    $stmt = $this->conn->prepare($query);
+
+    // sanitize
+    $this->id = htmlspecialchars(strip_tags($this->id));
+
+    // bind id of record to destroy
+    $stmt->bindParam(1, $this->id);
+
+    // execute query
+    if($stmt->execute()){
+      return true;
+    }
+
+    return false;
+  }
 }
