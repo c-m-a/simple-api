86d1e455cb79e46f77f2b0805c0e00e5d1a3d02c
Cmauricio Parra <cmauriciop@yahoo.com.mx>
2020-04-22 06:41:27 -0500

    Add: http method request to index file

--- citizens/index.php
+++ citizens/index.php
@@ -1,17 +1,18 @@
 <?php
+// required headers
+header('Access-Control-Allow-Origin: *');
+header('Content-Type: application/json; charset=UTF-8');
+header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE');
+header('Access-Control-Max-Age: 3600');
+header('Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With');
+
 $method = $_SERVER['REQUEST_METHOD'];
 
 $files['POST'] = 'create';
 $files['GET'] = 'read';
-$files['PUT'] = 'create';
+$files['PUT'] = 'update';
 $files['DELETE'] = 'destroy';
 
 $action = $files[$method];
 
-if (!empty($action)) {
-  require($action . '.php');
-} else {
-  echo json_encode(
-    array('message' => 'Request method not allowed!')
-  );
-}
+require($action . '.php');
