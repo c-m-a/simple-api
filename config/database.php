888218f2ecf19b08eb082c457e29942798218eb7
Cmauricio Parra <cmauriciop@yahoo.com.mx>
2020-04-22 02:17:35 -0500

    Fix: coding style bugs

--- config/database.php
+++ config/database.php
@@ -1,24 +1,23 @@
 <?php
 class Database {
 
-
   // specify your own database credentials
   private $host = 'localhost';
   private $db_name = 'api';
-  private $username = 'root';
-  private $password = '';
+  private $username = '';
+  private $passwd = '';
   private $driver = 'psql';
-  public $conn;
+  public $conn = '';
 
   // get the database connection
-  public function getConnection(){
+  public function getConnection() {
 
     $this->conn = null;
     $conn = $this->driver . ':host=' . $this->host .
       ';dbname=' . $this->db_name;
 
     try{
-      $this->conn = new PDO($conn, $this->username, $this->passwd;);
+      $this->conn = new PDO($conn, $this->username, $this->passwd);
       $this->conn->exec('set names utf8');
     }catch(PDOException $exception){
       echo 'Connection error: ' . $exception->getMessage();
