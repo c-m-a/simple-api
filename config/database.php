818635815a69333b53f767f12d0d13e8e42bc137
Cmauricio Parra <cmauriciop@yahoo.com.mx>
2020-04-22 01:18:27 -0500

    First commit API simple project on PHP

---
+++ config/database.php
@@ -0,0 +1,29 @@
+<?php
+class Database {
+
+
+  // specify your own database credentials
+  private $host = 'localhost';
+  private $db_name = 'api';
+  private $username = 'root';
+  private $password = '';
+  private $driver = 'psql';
+  public $conn;
+
+  // get the database connection
+  public function getConnection(){
+
+    $this->conn = null;
+    $conn = $this->driver . ':host=' . $this->host .
+      ';dbname=' . $this->db_name;
+
+    try{
+      $this->conn = new PDO($conn, $this->username, $this->passwd;);
+      $this->conn->exec('set names utf8');
+    }catch(PDOException $exception){
+      echo 'Connection error: ' . $exception->getMessage();
+    }
+
+    return $this->conn;
+  }
+}
